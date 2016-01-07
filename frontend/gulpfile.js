350742a48dc37910309ef1b1f94491ecc24924da
David Votrubec <david.votrubec@gmail.com>
2016-01-07 12:36:43 +0100

    updated gulpfile | new standalone tasks

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -1,5 +1,33 @@
 var gulp = require('gulp');
+var webserver = require('gulp-webserver');
 
-gulp.task('default', function () {
-    console.log('gupling');
-})
+var paths = {
+    temp: 'temp',
+    index: 'app/index.html'
+}
+
+gulp.task('default', ['tempSetup', 'serve'])
+
+/**
+ * prepare temp directory
+ */
+gulp.task('tempSetup', function(){
+    gulp.src(paths.index)
+    .pipe(gulp.dest(paths.temp));
+});
+
+/**
+ * servers static files
+ */
+gulp.task('serve', function(){
+    
+    gulp.src(paths.temp)
+    .pipe(webserver({
+        // open browser?
+        open: true,
+        
+        // if falsy then it opens the index file located in the .src()
+        //directoryListing: true
+    }));
+    
+});
