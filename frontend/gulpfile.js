fbb17679983da340a77aacf749499a146ea42f1f
David Votrubec <david.votrubec@gmail.com>
2016-01-07 13:11:20 +0100

    Added default bower.json file

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -1,29 +1,31 @@
 var gulp = require('gulp');
 var webserver = require('gulp-webserver');
 var mainBowerfiles = require('main-bower-files');
+var inject = require('gulp-inject');
 
 var paths = {
     temp: 'temp',
+    tempVendor: 'temp/vendor',
     index: 'app/index.html'
 }
 
-gulp.task('default', ['tempSetup', 'serve']);
+gulp.task('default', ['scripts', 'serve']);
 
 gulp.task('scripts', function(){
     
+    // copy the index file and get reference to it
+    var tempIndexFile = gulp.src(paths.index).pipe(gulp.dest(paths.temp));
+    
     // mainBowerfiles returns string (array) of installed bower components
     // each has a main files, as listed in the .bower.json
-    gulp.src(mainBowerfiles())
-    .pipe(gulp.dest())
-});
-
-/**
- * prepare temp directory
- */
-gulp.task('tempSetup', function(){
-    gulp.src(paths.index)
+    var tempVendors = gulp.src(mainBowerfiles()).pipe(gulp.dest(paths.tempVendor));
+    
+    // inject vendor files (js and css) into index file
+    tempIndexFile.pipe(inject(tempVendors))
+    // then replace the index file with its updated version
     .pipe(gulp.dest(paths.temp));
 });
+
 
 /**
  * servers static files
