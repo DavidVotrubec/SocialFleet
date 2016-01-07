316b61a58e2da0a3b8b11959a2460722388504f7
David Votrubec <david.votrubec@gmail.com>
2016-01-07 12:47:07 +0100

    Installed gulp-inject for injecting files into index.html

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -1,12 +1,21 @@
 var gulp = require('gulp');
 var webserver = require('gulp-webserver');
+var mainBowerfiles = require('main-bower-files');
 
 var paths = {
     temp: 'temp',
     index: 'app/index.html'
 }
 
-gulp.task('default', ['tempSetup', 'serve'])
+gulp.task('default', ['tempSetup', 'serve']);
+
+gulp.task('scripts', function(){
+    
+    // mainBowerfiles returns string (array) of installed bower components
+    // each has a main files, as listed in the .bower.json
+    gulp.src(mainBowerfiles())
+    .pipe(gulp.dest())
+});
 
 /**
  * prepare temp directory
